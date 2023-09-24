89d400380c3ec09f954491a74bad5f9d2d76840d
Biplab Biswas <biswasbiplab98@gmail.com>
2023-09-24 22:32:28 +0530

    removing test

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,5 @@
 import React from "react";
 
 export const Footer = () => {
-  return (
-    <div>
-      {true ? (
-        <a href="https://dogtinder.biswas.live/" target="_blank">
-          Click
-        </a>
-      ) : null}
-    </div>
-  );
+  return <div></div>;
 };
