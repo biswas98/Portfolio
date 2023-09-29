a9215dda394806418b8fd12dcce3a099d8ed1fec
Biplab Biswas <biswasbiplab98@gmail.com>
2023-09-29 22:45:14 +0530

    navbar visibility handling prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
+  const [click, setClick] = useState(false);
+  console.log(click);
   return (
     <>
-      <Navbar />
+      {click ? <Navbar /> : null}
+      <button className="btn-style" onClick={() => setClick(!click)}>
+        Click me
+      </button>
     </>
   );
 }
