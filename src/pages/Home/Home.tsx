7a0a70e28d930a3612c796684a0f26fbf65a5450
Biplab Biswas <biswasbiplab98@gmail.com>
2023-10-12 22:38:50 +0530

    leaning about famer motion

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
+import { motion } from 'framer-motion';
+
 import { Navbar } from '../../components';
 
 import './styles.css';
 
 export const Home = () => {
   const [click, setClick] = useState<boolean>(false);
+
   console.log(click);
   return (
     <div>
       <div className="nav_option_container">
         <button className="nav_option" onClick={() => setClick(!click)} />
-        {click ? <Navbar /> : null}
+        {click ? (
+          <motion.div animate={{ rotate: click ? 360 : 0 }}>
+            <Navbar />
+          </motion.div>
+        ) : null}
       </div>
     </div>
   );
