90b03bb942ef01a7fd53b8fd8679c111d1c935e9
Biplab Biswas <biswasbiplab98@gmail.com>
2023-10-30 20:55:19 +0530

    minor change

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -24,11 +24,6 @@ export const Navbar = () => {
             RESUME
           </a>
         </li>
-        <li className="list_item">
-          <a className="nav_anchor_style" href="#">
-            RESUME
-          </a>
-        </li>
       </ul>
     </div>
   );
